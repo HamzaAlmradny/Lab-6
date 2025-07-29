34c4ff434ffb10ef78046da564eb9aace0956b27
Dr. Alsayed <Dr. Alsayed@DESKTOP-APIOASQ>
2025-07-29 20:59:34 +0300

    Add project files.

---
+++ Lab 6/Lab 6.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	int num1, num2, sum, sub, div, mult, mod;
+
+	cout << "Please, enter the first number: ";
+	cin >> num1;
+
+	cout << "Please, enter the second number: ";
+	cin >> num2;
+
+	
+	sum = num1 + num2;
+
+	sub = num1 - num2;
+
+	div = num1 / num2;
+
+	mult = num1 * num2;
+
+	mod = num1 % num2;
+
+	cout << "The sum is " << sum << "\n";
+  
+	cout << "The sub is " << sub << "\n";
+
+	cout << "The div is " << div << "\n";
+
+	cout << "The mult is " << mult << "\n";
+
+	cout << "The mod is " << mod << "\n";
+
+
+	
+	
+	
+	
+	
+	return 0;
+
+}
+
